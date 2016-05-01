86c5459f88f38e187d0a25cf7cd83c8d280fb6fe
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-30 21:47:03 -0400

    Fixing setup.py version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from os.path import join
 
 setup(name="vy",
-      version="0.2",
+      version="0.0.1",
       description="A vim-like in python made from scratch.",
       packages=["vyapp", 
                 "vyapp.plugins",
@@ -17,7 +17,15 @@ setup(name="vy",
       scripts=['vy'],
       package_data={'vyapp': ['vyrc', join('vyapp', 'vyrc')]},
       author="Iury O. G. Figueiredo",
-      author_email="ioliveira@id.uff.br")
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/vy',
+      download_url='https://github.com/iogf/vy/releases',
+      keywords=['vy', 'vi', 'vim', 'emacs', 'sublime', 'atom', 'nano', 'vim-like'],
+      classifiers=[])
+ 
+
+
+
 
 
 
